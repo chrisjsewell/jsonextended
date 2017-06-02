2d450186100cd7e1845fef0c5b52de8dfa924c87
Chris Sewell <chrisj_sewell@hotmail.com>
2017-06-02 17:40:48 +0100

    fixed memory_usage func

--- jsonextended/utils.py
+++ jsonextended/utils.py
@@ -1,14 +1,31 @@
 def memory_usage():
     """return memory usage of python process in MB 
     
+    from http://fa.bianp.net/blog/2013/different-ways-to-get-memory-consumption-or-lessons-learned-from-memory_profiler/
+    
     >>> type(memory_usage())
-    float
+    <type 'float'>
     
     """
-    import resource, sys
-    rusage_denom = 1024.
-    if sys.platform == 'darwin':
-        # ... it seems that in OSX the output is different units ...
-        rusage_denom = rusage_denom * rusage_denom
-    mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / rusage_denom
+    try:
+        import psutil, os
+    except ImportError:
+        return _memory_usage_ps()
+        
+    process = psutil.Process(os.getpid())
+    mem = process.get_memory_info()[0] / float(2 ** 20)
+    return mem
+
+def _memory_usage_ps():
+    """return memory usage of python process in MB 
+    
+    >>> type(memory_usage_ps())
+    <type 'float'>
+    
+    """
+    import subprocess, os
+    out = subprocess.Popen(['ps', 'v', '-p', str(os.getpid())],
+    stdout=subprocess.PIPE).communicate()[0].split(b'\n')
+    vsz_index = out[0].split().index(b'RSS')
+    mem = float(out[1].split()[vsz_index]) / 1024
     return mem
