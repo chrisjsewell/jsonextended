2ba3baa3697d7500fdfef2b35d731a83474064d6
Chris Sewell <chrisj_sewell@hotmail.com>
2017-08-12 15:33:55 +0100

    bug fix to account for empty lists

--- jsonextended/edict.py
+++ jsonextended/edict.py
@@ -45,6 +45,8 @@ def is_dict_like(obj,attr=('keys','items')):
 
 def is_list_of_dict_like(obj,attr=('keys','items')):
     """test if object is a list only containing dict like items """
+    if not obj:
+        return False
     try:
         return all([is_dict_like(i,attr) for i in obj])
     except:
