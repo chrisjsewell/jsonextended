ae5463ed3b926e5720b91867a681a64f524df0af
Chris Sewell <chrisj_sewell@hotmail.com>
2017-08-16 04:44:14 +0100

    try adding analytics id to theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -124,7 +124,7 @@ todo_include_todos = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_theme_options = {'analytics_id':'UA-64089951-4'}
 
 # -- Options for HTMLHelp output ------------------------------------------
 
