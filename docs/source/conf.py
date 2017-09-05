6769a9914b97975054ddca6a9d11e9ac0a2a883b
Chris Sewell <chrisj_sewell@hotmail.com>
2017-09-06 00:54:20 +0100

    subheading for minor releases

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,27 +18,36 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('.'))
+
+# sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
 import jsonextended
 
 # create releases page
 import urllib
 import json
-git_history = urllib.request.urlopen('https://api.github.com/repos/chrisjsewell/jsonextended/releases').read().decode('utf-8')
-git_history_json = json.loads(git_history)
-with open('releases.rst', 'w') as f:
-	f.write('Releases\n')
-	f.write('---------\n')
-	f.write('\n')
-	for r in git_history_json:
-		f.write(' '.join([r['tag_name'],'-',r['name'],'\n']))
-		f.write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-		f.write('\n')
-		for line in r['body'].split('\n'):
-			f.write(' '.join([line, '\n']))
-		f.write('\n')
 
+git_history = urllib.request.urlopen('https://api.github.com/repos/chrisjsewell/jsonextended/releases').read().decode(
+    'utf-8')
+git_history_json = json.loads(git_history)
+
+current_maj_min = [0, 0]
+with open('releases.rst', 'w') as f:
+    f.write('Releases\n')
+    f.write('---------\n')
+    f.write('\n')
+    for rel in git_history_json:
+        rversion = [int(r) for r in rel['tag_name'].replace("v", "").split(".")]
+        f.write(' '.join([rel['tag_name'], '-', rel['name'], '\n']))
+        if rversion[0:2] != current_maj_min:
+            f.write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            current_maj_min = rversion[0:2]
+        else:
+            f.write('++++++++++++++++++++++++++++++++++++++++++')
+        f.write('\n')
+        for line in rel['body'].split('\n'):
+            f.write(' '.join([line, '\n']))
+        f.write('\n')
 
 # -- General configuration ------------------------------------------------
 
@@ -50,16 +59,16 @@ with open('releases.rst', 'w') as f:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-     'sphinx.ext.napoleon',
-     'sphinx.ext.autosummary']
+              'sphinx.ext.doctest',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.ifconfig',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -106,13 +115,12 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -124,13 +132,12 @@ todo_include_todos = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_theme_options = {'analytics_id':'UA-64089951-4'}
+html_theme_options = {'analytics_id': 'UA-64089951-4'}
 
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'jsonextendeddoc'
-
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -160,7 +167,6 @@ latex_documents = [
      u'Chris Sewell', 'manual'),
 ]
 
-
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
@@ -169,7 +175,6 @@ man_pages = [
     (master_doc, 'jsonextended', u'jsonextended Documentation',
      [author], 1)
 ]
-
 
 # -- Options for Texinfo output -------------------------------------------
 
@@ -181,7 +186,6 @@ texinfo_documents = [
      author, 'jsonextended', description,
      'Miscellaneous'),
 ]
-
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
@@ -197,16 +201,17 @@ napoleon_use_ivar = True
 napoleon_use_param = True
 napoleon_use_rtype = True
 
+
 # adapted from https://github.com/markovmodel/PyEMMA/blob/devel/doc/source/conf.py#L285
 # and discussed here: https://stackoverflow.com/questions/20569011/python-sphinx-autosummary-automated-listing-of-member-functions
 def setup(app):
-    #app.connect('autodoc-skip-member', skip_deprecated)
+    # app.connect('autodoc-skip-member', skip_deprecated)
     try:
         from sphinx.ext.autosummary import Autosummary
-        #from sphinx.ext.autosummary import get_documenter
+        # from sphinx.ext.autosummary import get_documenter
         from docutils.parsers.rst import directives
-        #from sphinx.util.inspect import safe_getattr
-        #import re
+        # from sphinx.util.inspect import safe_getattr
+        # import re
         import inspect
         from types import FunctionType
 
@@ -216,11 +221,11 @@ def setup(app):
                 'functions': directives.unchanged,
                 'classes': directives.unchanged,
                 'toctree': directives.unchanged,
-                'nosignatures':directives.unchanged
+                'nosignatures': directives.unchanged
             }
 
             required_arguments = 1
-                
+
             @staticmethod
             def get_functions(mod):
 
@@ -228,7 +233,7 @@ def setup(app):
                     return isinstance(obj, FunctionType) and obj.__module__ == mod.__name__
 
                 members = inspect.getmembers(mod, predicate=is_function_local)
-                return [name for name,value in members if not name.startswith('_')]
+                return [name for name, value in members if not name.startswith('_')]
 
             @staticmethod
             def get_classes(mod):
@@ -237,28 +242,28 @@ def setup(app):
                     return inspect.isclass(obj) and obj.__module__ == mod.__name__
 
                 members = inspect.getmembers(mod, predicate=is_class_local)
-                return [name for name,value in members if not name.startswith('_')]
+                return [name for name, value in members if not name.startswith('_')]
 
             def run(self):
-                
+
                 mod_path = self.arguments[0]
-                
+
                 (package_name, mod_name) = mod_path.rsplit('.', 1)
                 pkg = __import__(package_name, globals(), locals(), [mod_name])
                 mod = getattr(pkg, mod_name)
-            
+
                 if 'classes' in self.options:
                     klasses = self.get_classes(mod)
                     self.content = ["~%s.%s" % (mod_path, klass) for klass in klasses if not klass.startswith('_')]
-                if 'functions' in self.options:                    
+                if 'functions' in self.options:
                     functions = self.get_functions(mod)
                     content = ["~%s.%s" % (mod_path, func) for func in functions if not func.startswith('_')]
                     if self.content:
                         self.content += content
                     else:
                         self.content = content
-                try: 
-                    pass                   
+                try:
+                    pass
                 finally:
                     return super(AutoFunctionSummary, self).run()
 
